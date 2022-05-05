4d5442af607c6d11466046a2de461d6afd0d6055
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2022-05-05 08:59:02 +0000

    :tada:  Spin project

---
+++ lib/guru_cli.dart
@@ -0,0 +1,11 @@
+/// Guru Command Line Interface for Project Setup
+///
+/// ```sh
+/// # activate guru_cli
+/// dart pub global activate guru_cli
+///
+/// # Learn how to use the cli
+/// guru --help
+/// ```
+
+library guru_cli;
---
+++ test/guru_cli_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Init --', () {
+    ///TODO: Add new test here
+  });
+}
